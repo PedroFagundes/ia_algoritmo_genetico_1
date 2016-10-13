aaca0a68efb7e02cac018cbacbd3e0bd4f72dbb8
Diemerson <Diemerson.ti@gmail.com>
2016-10-13 16:20:44 -0300

    Release da primeira versao. Funcionando

--- application/views.py
+++ application/views.py
@@ -4,6 +4,13 @@ from random import randint
 from django.shortcuts import render
 
 from .forms import Formulario
+ 
+def get_letras_unicas(palavras):
+	letras_unicas = []
+	for l in palavras:
+		if l not in letras_unicas:
+			letras_unicas.append(l) 
+	return letras_unicas
 
 
 def gera_populacao(qtd):
@@ -19,6 +26,28 @@ def gera_populacao(qtd):
 		individuo = ''
 	return populacao
 
+def gera_fa(populacao, letras_unicas, palavras):
+	fa_palavra = ''
+	fa = ''
+	vetor_individuo_fa = []
+	vetor_fa = []
+	populacao_com_fa = []
+	for individuo in populacao:
+		for palavra in palavras:
+			for p in palavra:
+				index_letra = letras_unicas.index(p)
+				fa_palavra += individuo[index_letra]
+			vetor_individuo_fa.append(fa_palavra)
+			fa_palavra = ''
+		fa = abs(int(vetor_individuo_fa[0]) + int(vetor_individuo_fa[1]) - int(vetor_individuo_fa[2]))
+		vetor_individuo_fa = []
+		vetor_fa.append(str(fa))
+		individuo_gerado = {}
+		individuo_gerado['individuo'] = individuo
+		individuo_gerado['fa'] = fa
+		populacao_com_fa.append(individuo_gerado)
+	#import pdb; pdb.set_trace()
+	return populacao_com_fa
 
 def index(request):
 	form = Formulario()
@@ -28,8 +57,19 @@ def index(request):
 		if form.is_valid():
 
 			# Gerando populacao
+			palavras = [form.cleaned_data['palavra_1'], form.cleaned_data['palavra_2'], form.cleaned_data['palavra_3']]
+
+			letras_unicas = get_letras_unicas(form.cleaned_data['palavra_1'] + form.cleaned_data['palavra_2'] + form.cleaned_data['palavra_3'])
+			
 			populacao = gera_populacao(int(form.cleaned_data['populacao']))
-			return render(request, 'application/resultado.html', {'populacao': populacao})
+
+			populacao = gera_fa(populacao, letras_unicas, palavras)
+
+			#import pdb; pdb.set_trace()
+
+			context = {'populacao' : populacao}
+
+			return render(request, 'application/resultado.html', context)
 
 		else:
 			return render(request, 'application/index.html', {'form': form})
